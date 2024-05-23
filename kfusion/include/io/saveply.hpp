c2a7f739037d78ff54d79b311302fb190436f99e
John <jnwangcsc@qq.com>
2024-05-23 13:45:55 +0800

    add save to ply

---
+++ kfusion/include/io/saveply.hpp
@@ -0,0 +1,20 @@
+#ifndef HEADER_SAVEPLY
+#define HEADER_SAVEPLY
+#include <string>
+using namespace std;
+//save points to ply
+void saveToPly(std::vector<Eigen::Vector3d> &vertices, string name)
+{
+    cout<<"saving the point cloud to the file"<<endl;
+    ofstream fscan("../results/"+name);
+    fscan<<"ply"<<endl<<"format ascii 1.0"<<endl<<"comment Created by Wang Junnan"<<endl;
+    fscan<<"element vertex "<<vertices.size()<<endl;
+    fscan<<"property float x"<<endl<<"property float y"<<endl<<"property float z"<<endl;
+    fscan<<"end_header"<<endl;
+
+    for(int i=0;i<vertices.size();i++){
+        fscan<<vertices[i][0]<<" "<<vertices[i][1]<<" "<<vertices[i][2]<<endl;
+    }
+    fscan.close();
+}
+#endif
